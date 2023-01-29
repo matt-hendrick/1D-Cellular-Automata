7b045f6ba2971f4d141df275067a2a0766cdb573
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2023-01-29 15:03:16 -0500

    Parameterize row size and allow toggling random start. Renamed some things. Add README and gif displaying terminal output.

---
+++ README.md
@@ -0,0 +1,19 @@
+# 1d Cellular Automata in the Command Line
+
+![1D Cellular Automata GIF](1DCellularAutomataDemo.gif)
+
+This is a simple CLI app that can display evolutions of one-dimensional cellular automata. For more info on 1D cellular automata and for visualizations of the outputs of the 256 rules, see [this Wolfram article](https://mathworld.wolfram.com/ElementaryCellularAutomaton.html)
+
+Three optional parameters can be passed in to affect the displayed output in the terminal.
+
+## First Parameter (usize) = Evolution Rule (0-255)
+
+A number between 0-255 can be passed in. This will have the automata evolve based upon the corresponding binary equivalent. If no parameter is provided, a random rule will be chosen.
+
+## Second Parameter (u32) = Size of the Vector to Display in the Terminal
+
+A number specifying the size of the vector that is displayed in the terminal. If no parameter is provided, a vector with 155 elements will be used.
+
+## Third Parameter (bool) = Randomize Starting Vector Values
+
+If a parameter is passed in, the starting vector will have a random mix of 0s and 1s. If no parameter is provided, only the vector's midpoint will be set to 1 (all other elements will be 0).
--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,21 @@ use std::env::args;
 use std::{thread, time};
 
 fn main() {
-    let mut current_vec = generate_starting_vec(155);
-    let chosen_rule_set = parse_rule_from_args();
+    let mut current_row: Vec<u32> = generate_starting_row();
+    let chosen_rule_set: u32 = parse_rule_from_args();
     println!(
         "Rule set = {}. Binary = {chosen_rule_set:08b}",
         chosen_rule_set
     );
-    let rule_set = get_rule_set(chosen_rule_set);
+    let rule_set: HashMap<String, u32> = get_rule_set(chosen_rule_set);
     println!("Hashset = {:?}", rule_set);
-    print_row(&current_vec);
+    print_row(&current_row);
     loop {
         let duration = time::Duration::from_millis(100);
 
         thread::sleep(duration);
-        current_vec = calc_new_vec(current_vec, &rule_set);
-        print_row(&current_vec);
+        current_row = calc_new_row(current_row, &rule_set);
+        print_row(&current_row);
     }
 }
 
@@ -39,7 +39,7 @@ fn parse_rule_from_args() -> u32 {
         println!("No rule set specified. A random rule set will be chosen.");
         rand::thread_rng().gen_range(0..256)
     } else {
-        let parsed = args().nth(1).unwrap().parse::<u32>();
+        let parsed: Result<u32, std::num::ParseIntError> = args().nth(1).unwrap().parse::<u32>();
         if parsed.is_ok() {
             let result = parsed.unwrap();
             if result <= 256 {
@@ -53,19 +53,52 @@ fn parse_rule_from_args() -> u32 {
     }
 }
 
-fn generate_starting_vec(size: usize) -> Vec<u32> {
-    // let mut new_vec: Vec<u32> = Vec::new();
+fn parse_row_size_from_args() -> usize {
+    if args().len() < 3 {
+        println!("No vec size specified. Vec will be set to to a size of 155.");
+        155
+    } else {
+        let parsed: Result<usize, std::num::ParseIntError> =
+            args().nth(2).unwrap().parse::<usize>();
+        if parsed.is_ok() {
+            parsed.unwrap()
+        } else {
+            155
+        }
+    }
+}
 
-    // for _ in 1..size + 1 {
-    //     let randomish = rand::thread_rng().gen_range(0..2);
-    //     new_vec.push(randomish);
-    // }
+fn parse_randomize_starting_row_from_args() -> bool {
+    if args().len() < 4 {
+        println!("No parameter passed in to randomize the starting vec. Starting vec will have a single filled in value at the midpoint.");
+        false
+    } else {
+        println!(
+            "Parameter passed in to randomize the starting vec. Starting vec will be pseudo-random"
+        );
+        true
+    }
+}
 
-    let mut new_vec: Vec<u32> = vec![0; size];
-    let midpoint = ((new_vec.len() / 2) as f32).floor();
-    new_vec[midpoint as usize] = 1;
+fn generate_starting_row() -> Vec<u32> {
+    let size: usize = parse_row_size_from_args();
+    let random_start: bool = parse_randomize_starting_row_from_args();
 
-    return new_vec;
+    let mut starting_row: Vec<u32>;
+
+    if random_start {
+        starting_row = Vec::new();
+
+        for _ in 1..size + 1 {
+            let randomish = rand::thread_rng().gen_range(0..2);
+            starting_row.push(randomish);
+        }
+    } else {
+        starting_row = vec![0; size];
+        let midpoint = ((starting_row.len() / 2) as f32).floor();
+        starting_row[midpoint as usize] = 1;
+    }
+    return starting_row;
 }
 
 fn get_u32_at_binary_string_index(binary_string: &String, index: usize) -> u32 {
@@ -117,31 +150,29 @@ fn get_rule_set(rule_set: u32) -> HashMap<String, u32> {
     return hashmap;
 }
 
-fn calc_new_vec(old_vec: Vec<u32>, rule_set: &HashMap<String, u32>) -> Vec<u32> {
-    let mut new_vec: Vec<u32> = Vec::new();
+fn calc_new_row(old_row: Vec<u32>, rule_set: &HashMap<String, u32>) -> Vec<u32> {
+    let mut new_row: Vec<u32> = Vec::new();
 
-    for (i, num) in old_vec.iter().enumerate() {
+    for (i, num) in old_row.iter().enumerate() {
         let mut curr_key: String = "".to_string();
         if i == 0 {
-            curr_key += &old_vec[old_vec.len() - 1].to_string();
+            curr_key += &old_row[old_row.len() - 1].to_string();
         } else {
-            curr_key += &old_vec[i - 1].to_string();
+            curr_key += &old_row[i - 1].to_string();
         }
 
         curr_key += &num.to_string();
 
-        if i == old_vec.len() - 1 {
-            curr_key += &old_vec[0].to_string();
+        if i == old_row.len() - 1 {
+            curr_key += &old_row[0].to_string();
         } else {
-            curr_key += &old_vec[i + 1].to_string();
+            curr_key += &old_row[i + 1].to_string();
         }
 
-        let new_val = rule_set.get(&curr_key).unwrap();
+        let new_val: &u32 = rule_set.get(&curr_key).unwrap();
 
-        // println!("{}, {}, {}, {}", i, num, curr_key, new_val);
-
-        new_vec.push(*new_val);
+        new_row.push(*new_val);
     }
 
-    return new_vec;
+    return new_row;
 }
